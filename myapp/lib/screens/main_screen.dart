4fb69fda588b988c38ebc9b8069ce203093273bb
panda <nbk.01409@gmail.com>
2020-03-12 15:00:46 +0900

    profile update

--- myapp/lib/screens/main_screen.dart
+++ myapp/lib/screens/main_screen.dart
@@ -5,15 +5,6 @@ import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -25,11 +16,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -99,8 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
 //              Text("© Allrights Reserved")
             ],
           ),
-          Container(),
-          Container()
+          Text("お気に入りの撮影写真を貼る予定", textAlign: TextAlign.center,),
+          Text("有能な記事を書く予定", textAlign: TextAlign.center,),
         ]),
       ),
     ));
